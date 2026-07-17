a2bb94d0c30249911946d61839d339fb9bfe7ab7
YuriVictoria <yuri.victoria@edu.ufes.br>
2026-07-17 01:24:18 -0300

    q9, q10, q11, q12, q13

--- lista.pl
+++ lista.pl
@@ -1,5 +1,4 @@
 % QUESTÃO 1
-
 % a)
 % pai(Pai, Filho(a))
 pai(ivo,eva).
@@ -64,17 +63,15 @@ avô(X,Y) :- homem(X), pai(X,Z), (pai(Z,Y);mae(Z,Y)).
 avó(X,Y) :- mulher(X), mae(X,Z), (pai(Z,Y);mae(Z,Y)).
 
 % QUESTÃO 2
-
 % a)
 % feliz(Pessoa)
 feliz(X) :- pai(X,_);mae(X,_).
 
 % b)
 % casal(Pessoa 1, Pessoa 2)
-casal(X,Y) :- gerou(X,Z),gerou(Y,Z),!.
+casal(X,Y) :- gerou(X,Z), gerou(Y,Z), X \= Y.
 
 % QUESTÃO 3
-
 % país(Nome, Área, População)
 país(brasil, 9, 130).
 país(china, 12, 1800).
@@ -90,7 +87,6 @@ densidade(P,D) :- país(P, Ar, Pop), D is Pop/Ar.
 maisPopuloso(P1,P2) :- país(P1, _, Pop1), país(P2, _, Pop2), Pop1>Pop2.
 
 % QUESTÃO 4
-
 pessoa(ana, f, 23, 1.55, 56.0).
 pessoa(bia, f, 19, 1.71, 61.3).
 pessoa(ivo, m, 22, 1.80, 70.5).
@@ -99,16 +95,15 @@ pessoa(eva, f, 28, 1.75, 68.7).
 pessoa(ary, m, 25, 1.72, 68.9).
 
 % a)
-% ?- pessoa(Nome,f,Idade,_,_),Idade>20
+% ?- pessoa(Nome,f,Idade,_,_),Idade>20.
 
 % b)
-% ?- pessoa(Nome,f,_,Altura,Peso),Altura>=1.70,Peso<65
+% ?- pessoa(Nome,_,_,Altura,Peso),Altura>=1.70,Peso<65.
 
 % c)
-% ?- pessoa(X,_,_,AlturaX,_),pessoa(Y,_,_,AlturaY,_),casal(X,Y),((homem(X),AlturaX>AlturaY);(homem(Y),AlturaY>AlturaX))
+% ?- pessoa(X,m,_,AlturaX,_),pessoa(Y,f,_,AlturaY,_),casal(X,Y),(AlturaX>AlturaY).
 
 % QUESTÃO 5
-
 joga(ana,volei).
 joga(bia,tenis).
 joga(ivo,basquete).
@@ -172,23 +167,57 @@ joga(leo,tenis).
 % pode ser percebido na árvore.
 
 % QUESTÃO 6
-
 num(N,positivo) :- N>0,!.
 num(0,nulo) :- !.
 num(N,negativo) :- N<0.
 
 % QUESTÃO 7
-
 mult(_,0,0). % Base
 mult(X,Y,P_atual) :- Y>0, M is Y-1, mult(X,M,P_ant), P_atual is X+P_ant. % Passo
 
 % QUESTÃO 8
-
 bin(Num, Res) :- Num < 2, Res is Num. % Base
 bin(N, R) :- N >= 2, Temp is mod(N,2), bin(N//2, R_antes), R is (R_antes*10)+Temp. % Passo
 
 % QUESTÃO 9
+% a)
+estrada(a,b,25).
+estrada(a,d,23).
+
+estrada(b,c,19).
+estrada(b,e,32).
+
+estrada(c,d,14).
+estrada(c,f,28).
+
+estrada(d,f,30).
+
+estrada(e,f,26).
+
+% b)
+dist(Origem,Destino,Dist) :- estrada(Origem, Destino, Dist);estrada(Origem, Caminho, D1), dist(Caminho, Destino, D2), Caminho \= Destino, Dist is D1+D2.
+
+% c)
+rota(Origem, Destino, [Origem, Destino]) :- estrada(Origem, Destino, _).
+rota(Origem, Destino, [Origem | RestoRota]) :- estrada(Origem, Caminho, _), rota(Caminho, Destino, RestoRota).
+
+% QUESTÃO 10 
+ultimo([H|[]],H).
+ultimo([_|T],U) :- ultimo(T, U).
+
+% QUESTÃO 11
+max([H|[]],H).
+max([H|T],U) :- max(T, A), ((H >= A, U is H);(A > H, U is A)).
+
+% QUESTÃO 12
+anexa([],R,R).
+anexa([H|T],L2, [H|L]) :- anexa(T,L2,L).
+
+% QUESTÃO 13
+inv([], []).
+inv([H|T], R) :- inv(T, CaudaInvertida), anexa(CaudaInvertida, [H], R).
+
+% QUESTÃO 14
 
 
-
-% Sentido único
+% QUESTÃO 15
